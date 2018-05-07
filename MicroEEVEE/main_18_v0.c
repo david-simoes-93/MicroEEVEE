833ce07592f71d4eeeb8055a13e69dcf394e77de
bluemoon93 <david.simoes@ua.pt>
2018-05-07 23:16:58 +0100

    no warnings

--- MicroEEVEE/main_18_v0.c
+++ MicroEEVEE/main_18_v0.c
@@ -1,8 +1,5 @@
 #include "mr32.h"
-#include <math.h>
-#include <string.h>
 #include <stdlib.h>
-
 
 
 #include "MazeMap.h" //maze[][]
@@ -14,7 +11,6 @@
 #include "StopAtBeacon.h"
 #include "Wander.h"
 #include "Sensors.h"
-#include "bluetooth_comm.h"
 
 
 /* --------- Functions -------------*/
@@ -22,26 +18,21 @@ void reactive_decide();
 
 double get_next_dir(int sx, int sy);
 
-bool compare_points(int * p, int*q){
-    if(p[0] == q[0] && p[1]== q[1]) return true;
+bool compare_points(int *p, int *q) {
+    if (p[0] == q[0] && p[1] == q[1])
+        return true;
     return false;
 }
 
 void get_point_from_list();
 
 /* --------- Variables -------------*/
-
-int count_beacon;
-
 int beaconPoint[2]; //in cm
 int old_beaconPoint[2];
 double dir_beacon, go_to_dir;
-bool recalculate;
-
+bool should_recalculate_astar;
 int lastBeh;
-
 int paths[cols][rows][2]; //in cells
-
 int path_list[100][2]; //in cells
 int path_length = 0;
 
@@ -71,16 +62,13 @@ int main(void) {
     }
 
     /*variables*/
-    count_beacon = 0;
-    int count_ticks;
-    count_ticks = 0;
-
+    int count_ticks = 0;
     init_maze();
     int c = 0;
-
-    recalculate = false;
+    should_recalculate_astar = false;
     returning_home = 0; //only true when beacon is reached
-    bool follow_astar_path = true;
+    bool follow_astar_path = false;
+
     /*-------------------*/
 
     while (!stopButton()) {
@@ -96,30 +84,30 @@ int main(void) {
         printf("Obst_left=%03d, Obst_center=%03d, Obst_right=%03d, Bat_voltage=%03d\n", analogSensors.obstSensLeft,
                analogSensors.obstSensFront, analogSensors.obstSensRight, analogSensors.batteryVoltage);
 
-        gndVals[c] = groundSensor;	c=(c+1)%5; //buffer
+        gndVals[c] = groundSensor;
+        c = (c + 1) % 5; //buffer
 
         /* Track robot position and orientation */
 
         getRobotPos_int(&x, &y, &t);
 
         //update map
-        update_map(x, y, analogSensors.obstSensLeft, analogSensors.obstSensFront, analogSensors.obstSensRight,t);
+        update_map(x, y, analogSensors.obstSensLeft, analogSensors.obstSensFront, analogSensors.obstSensRight, t);
 
 
         //Find beacon
-        if ( servoControl() ){ //found beacon
+        if (servoControl()) { //found beacon
             //dir_beacon = beaconDir;
             /*calculate beacon*/
             old_beaconPoint[0] = beaconPoint[0];
             old_beaconPoint[1] = beaconPoint[1];
-            int tmp[2] = {x,y};
+            int tmp[2] = {x, y};
 
             //add new Line and get Avg Beacon Point
-            if (getDirectionTarget(tmp, dir_beacon, beaconPoint) ){ //i have a beacon point at beaconPoint
-                if (! compare_points(old_beaconPoint, beaconPoint)) recalculate = true;
+            if (getDirectionTarget(tmp, dir_beacon, beaconPoint)) { //i have a beacon point at beaconPoint
+                if (!compare_points(old_beaconPoint, beaconPoint)) should_recalculate_astar = true;
                 follow_astar_path = true;
-            }
-            else{ //no intersect was made, only have direction!
+            } else { //no intersect was made, only have direction!
                 //go_to_dir = beaconDir;
                 follow_astar_path = false;
             }
@@ -133,19 +121,18 @@ int main(void) {
         int sx = tmp[0];
         int sy = tmp[1];
 
-        if(recalculate){
-            //recalculate new path
-            if(returning_home){ //beacon was found --> go Home
+        if (should_recalculate_astar) {
+            //should_recalculate_astar new path
+            if (returning_home) { //beacon was found --> go Home
                 tmp[0] = 7; //in cells
                 tmp[1] = 7;
-            }
-            else{
+            } else {
                 get_cell_index_from_gps_coords(beaconPoint[0], beaconPoint[1], tmp);
             }
             int gx = tmp[0];
             int gy = tmp[1];
 
-            if( astar(rows, cols, sx,sy,gx, gy, paths)){
+            if (astar(rows, cols, sx, sy, gx, gy, paths)) {
                 printf("Astar search %d\n", true);
                 while (true) { //get Path_list
                     path_list[path_length][0] = paths[gx][gy][0];
@@ -163,28 +150,27 @@ int main(void) {
                 get_point_from_list();
                 //beaconDir = get_next_dir(sx, sy); //in cells
 
-            }
-            else{
+            } else {
                 printf("Cannot find path to goal (%d, %d) \n", gx, gy);
                 break;
             }
 
-            recalculate = false;
+            should_recalculate_astar = false;
         }
 
 
-        if (sx == path_list[path_length-1][0] && sy == path_list[path_length-1][1] ){
-            if(returning_home && sx == 0 && sy == 0) {
+        if (sx == path_list[path_length - 1][0] && sy == path_list[path_length - 1][1]) {
+            if (returning_home && sx == 0 && sy == 0) {
                 //reach Home
                 printf("Found Home");
                 break;
             }
-            path_length = path_length-1;
+            path_length = path_length - 1;
             get_point_from_list();
             //beaconDir = get_next_dir(sx, sy);
         }
 
-        if(follow_astar_path){
+        if (follow_astar_path) {
             followPoints_(); //update beaconDir every iteration
         }
         //else beaconPoint from servoControl is executed
@@ -196,35 +182,38 @@ int main(void) {
 
 
     disableObstSens();
-    setVel2(0,0);
+    setVel2(0, 0);
     int frequency = 4; //Hz
-    while(true){
+    while (true) {
         leds(0x00);
-        wait(1/frequency/10);
+        wait(1 / frequency / 10);
         leds(0xFF);
-        wait(1/frequency/10);
+        wait(1 / frequency / 10);
     }
     return 0;
 
 }
-//update LastPointX LastPointY
-void get_point_from_list(){
-    int target_path_point_index = path_length-1;
-    if (path_length > 1)
-        target_path_point_index = path_length-2;
 
-    get_middle_coords_from_index(path_list[target_path_point_index][0], path_list[target_path_point_index][1], &lastPointX, &lastPointY);
+//update LastPointX LastPointY
+void get_point_from_list() {
+    int target_path_point_index = path_length - 1;
+    if (path_length > 1)
+        target_path_point_index = path_length - 2;
+
+    get_middle_coords_from_index(path_list[target_path_point_index][0], path_list[target_path_point_index][1],
+                                 &lastPointX, &lastPointY);
 
 }
 
 // dir in DEGREES
-double get_next_dir(int sx, int sy){
+double get_next_dir(int sx, int sy) {
     // define beaconDir
-    int target_path_point_index = path_length-1;
+    int target_path_point_index = path_length - 1;
     if (path_length > 1)
-        target_path_point_index = path_length-2;
+        target_path_point_index = path_length - 2;
     return -normalizeAngle(get_target_dir(sx, sy, t,
-                          path_list[target_path_point_index][0], path_list[target_path_point_index][1])) * (180 / PI);
+                                          path_list[target_path_point_index][0],
+                                          path_list[target_path_point_index][1])) * (180 / PI);
 }
 
 void reactive_decide() {
@@ -232,42 +221,38 @@ void reactive_decide() {
     //printf("X= %f Y= %f Compass= %f\n", x, y, compass);
     prep_ftw();
 
-    if(sab_isPossible()){
+    if (sab_isPossible()) {
         //all light on
         leds(0xFF);
-        recalculate = true; //now go home
+        should_recalculate_astar = true; //now go home
         sab_execute();
-        if(lastBeh!=0){
+        if (lastBeh != 0) {
             printf("\tExecuting behaviour %s\n", sab_getName());
-            lastBeh=0;
+            lastBeh = 0;
         }
-    }
-    else if(ftw_isPossible()){
+    } else if (ftw_isPossible()) {
         ftw_execute();
-        if(lastBeh!=1){
+        if (lastBeh != 1) {
             printf("\tExecuting behaviour %s\n", ftw_getName());
-            lastBeh=1;
+            lastBeh = 1;
         }
-    }
-    else if(ftb_isPossible()){
+    } else if (ftb_isPossible()) {
         ftb_execute();
-        if(lastBeh!=2){
+        if (lastBeh != 2) {
             printf("\tExecuting behaviour %s\n", ftb_getName());
-            lastBeh=2;
+            lastBeh = 2;
         }
-    }
-    else if(do_isPossible()){
+    } else if (do_isPossible()) {
         do_execute();
-        if(lastBeh!=3){
+        if (lastBeh != 3) {
             printf("\tExecuting behaviour %s\n", do_getName());
-            lastBeh=3;
+            lastBeh = 3;
         }
-    }
-    else if(wan_isPossible()){
+    } else if (wan_isPossible()) {
         wan_execute();
-        if(lastBeh!=4){
+        if (lastBeh != 4) {
             printf("\tExecuting behaviour %s\n", wan_getName());
-            lastBeh=4;
+            lastBeh = 4;
         }
     }
 }
