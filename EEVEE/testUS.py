77f5494cb7d801bc7e3c1dd25f54eaaf524d61e6
bluemoon93 <david.simoes@ua.pt>
2018-07-31 13:53:52 +0100

    Docs added. US tested in 4 diff configurations, linear is best, but wasted an entire core.

--- EEVEE/testUS.py
+++ EEVEE/testUS.py
@@ -48,6 +48,7 @@ class USSensor:
 
         # speed of sound is 343.4m/s, 20ºC from WolframAlpha
         # since we have a 2-trip movement, we divide that by 2, giving us 171.7m/s
+        print(ping-start, pong-ping)
         return (pong - ping) * 171.7
 
 
@@ -102,7 +103,7 @@ class USSensorCallbacks:
             # speed of sound is 343.4m/s, 20ºC from WolframAlpha
             # since we have a 2-trip movement, we divide that by 2, giving us 171.7m/s
             self.dist = (time.time() - self.ping) * 171.7
-            print(self.dist)
+            print(min(max(0.03, self.dist), 4))
             self.get()
             # wake up trigger thread
             #self.waiting_for_pong = False
@@ -147,22 +148,93 @@ class USSensorInterrupts:
         return (time.time() - ping) * 171.7
 
 
+# UltraSound sensor
+class USSensorAsynch:
+    # In Board mode, the TRIG and ECHO pin numbers
+    def __init__(self, trig, echo):
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(trig, GPIO.OUT)
+        GPIO.setup(echo, GPIO.IN)
+        GPIO.add_event_detect(echo, GPIO.FALLING, self.read_ping)
+        self.trig_event = threading.Event()
+        self.echo_event = threading.Event()
+
+        self.limit = 2
+        self.echo = echo
+        self.trig = trig
+
+        self.dist = None
+        self.ping = None
+        self.start = None
+
+    # Get a distance measure (m) or None. Takes up to 1s (but usually less than 0.5s)
+    def get(self):
+        # enter mutex
+        self.trig_event.clear()
+        self.echo_event.clear()
+        self.dist = 4
+
+        # Send a trigger for 1ms
+        GPIO.output(self.trig, GPIO.HIGH)
+        time.sleep(0.001)
+        self.start = time.time()
+        GPIO.output(self.trig, GPIO.LOW)
+
+        # Wait up to 1ms for the ping to start (usually takes about 0.5ms)
+        self.ping = self.start
+        while self.ping - self.start < 0.001:
+            self.ping = time.time()
+            if GPIO.input(self.echo):
+                break
+        else:
+            self.echo_event.set()
+            return None
+
+        # wait for echo
+        self.echo_event.set()
+        self.trig_event.wait(timeout=self.limit)
+        return self.dist
+
+    def read_ping(self, _):
+        # save the value as soon as possible
+        pong = time.time()
+
+        # enter mutex
+        self.echo_event.wait()
+
+        # speed of sound is 343.4m/s, 20ºC from WolframAlpha
+        # since we have a 2-trip movement, we divide that by 2, giving us 171.7m/s
+        self.dist = (pong - self.ping) * 171.7
+
+        # wake up original thread
+        self.trig_event.set()
+
+
 def main():
+    # Asynchronous, using concurrency devices
+    if True:
+        us0 = USSensorAsynch(12, 16)
+        while True:
+            #us0.get()
+            print(min(max(0.03, us0.get()), 4))
+
     # Asynchronous, no callbacks
-    #us0 = USSensorInterrupts(12, 16)
-    #while True:
-    #    print(us0.get())
+    if False:
+        us0 = USSensorInterrupts(12, 16)
+        while True:
+            print(min(max(0.03, us0.get()), 4))
 
     # Asynchronous, using callbacks
-    us0 = USSensorCallbacks(12, 16)
-    us0.get()
-    #while True:
-    #    print(us0.get())
+    if False:
+        us0 = USSensorCallbacks(12, 16)
+        us0.get()
 
     # Synchronous, no interrupts
-    us0 = USSensor(12, 16)
-    while True:
-        print(us0.get())
+    if True:
+        us0 = USSensor(12, 16)
+        while True:
+            print(min(max(0.03, us0.get()), 4))
 
 
 main()
